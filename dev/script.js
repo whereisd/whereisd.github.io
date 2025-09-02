c17bf667749a15b0a08286aa3f6c0cae7c778a22
whereisd <wheredhike@gmail.com>
2025-09-02 13:09:10 -0700

    .

--- dev/script.js
+++ dev/script.js
@@ -30,7 +30,8 @@ map.setView([0, 0], 17);
 // }).addTo(map);
 
 // *****Mapbox Standard *****
-L.tileLayer('https://api.mapbox.com/styles/v1/whereisd/cmf2t86hy00to01rk5gllbu6r/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2hlcmVpc2QiLCJhIjoiY21mMnQybm9rMDRmbTJrcTljcWg2NzViZCJ9.suwjyVDj4jKzzCW-DHwDbA', {
+  'https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/256/{z}/{x}/{y}?access_token=YOUR_MAPBOX_ACCESS_TOKEN',
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2hlcmVpc2QiLCJhIjoiY21mMnQybm9rMDRmbTJrcTljcWg2NzViZCJ9.suwjyVDj4jKzzCW-DHwDbA', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> © <a href="https://www.mapbox.com/about/maps/">Mapbox</a> <strong><a href="https://labs.mapbox.com/contribute/" target="_blank">Improve this map</a></strong>'
 }).addTo(map);
